c83af6770253c0f4d45158daa573f5b7fc650b11
Rida22 <rida.moussaoui@telecomnancy.eu>
2022-12-03 17:26:18 +0100

    [EDIT] fixing the bug of VisitorTest

--- AntlrMavenProject/chaoscompiler/src/main/java/eu/tn/chaoscompiler/ast/VisitorTest.java
+++ AntlrMavenProject/chaoscompiler/src/main/java/eu/tn/chaoscompiler/ast/VisitorTest.java
@@ -1,8 +1,13 @@
 package eu.tn.chaoscompiler.ast;
 
+import eu.tn.chaoscompiler.ast.nodes.looporcondition.For;
+import eu.tn.chaoscompiler.ast.nodes.looporcondition.IfThenElse;
+import eu.tn.chaoscompiler.ast.nodes.looporcondition.Let;
+import eu.tn.chaoscompiler.ast.nodes.looporcondition.While;
 import eu.tn.chaoscompiler.ast.nodes.operators.Addition;
 import eu.tn.chaoscompiler.ast.nodes.operators.BinariesOperators;
 import eu.tn.chaoscompiler.ast.nodes.operators.Negation;
+import eu.tn.chaoscompiler.ast.nodes.references.FunctionCall;
 import eu.tn.chaoscompiler.ast.nodes.terminals.Id;
 import eu.tn.chaoscompiler.ast.nodes.Program;
 import eu.tn.chaoscompiler.ast.nodes.terminals.IntegerNode;
@@ -31,6 +36,31 @@ public class VisitorTest implements AstVisitor<String> {
 
     @Override
     public String visit(IntegerNode node) {
+        return null;
+    }
+
+    @Override
+    public String visit(FunctionCall node) {
+        return null;
+    }
+
+    @Override
+    public String visit(For forExpr) {
+        return null;
+    }
+
+    @Override
+    public String visit(While whileExpr) {
+        return null;
+    }
+
+    @Override
+    public String visit(IfThenElse ifThenElseExpr) {
+        return null;
+    }
+
+    @Override
+    public String visit(Let letExpr) {
         return null;
     }
 }
