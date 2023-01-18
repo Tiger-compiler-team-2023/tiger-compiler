9d1a3043f1d192b91ba106ea3859e5eae1f43285
Theo.Goureau <Theo.Goureau@telecomnancy.eu>
2023-01-18 16:38:39 +0100

    [ADD][TSem] Ajout d'un exemple de test sémantique

---
+++ AntlrMavenProject/chaoscompiler/go-mutrecrec.sh
@@ -0,0 +1,3 @@
+mvn clean -f "/home/theog/Documents/PCL1/pcl-goureau1u/AntlrMavenProject/chaoscompiler/pom.xml"
+mvn compile -f "/home/theog/Documents/PCL1/pcl-goureau1u/AntlrMavenProject/chaoscompiler/pom.xml"
+mvn exec:java -Dexec.mainClass=eu.tn.chaoscompiler.main.Main -Dexec.args="./src/test/ressources/correct/mutualRecursRecord.tig"
